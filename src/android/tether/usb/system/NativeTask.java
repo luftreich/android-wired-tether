241833751722651e50765daa06bda7aea1f12c82
harald.mue <harald.mue@428afd14-c53f-11de-a2e8-8d36a3108c74>
2009-11-06 00:14:33 +0000

    Nativelibrary-install workaround.

--- src/android/tether/usb/system/NativeTask.java
+++ src/android/tether/usb/system/NativeTask.java
@@ -3,17 +3,19 @@ package android.tether.usb.system;
 import android.util.Log;
 
 public class NativeTask {
-    static {
+    
+	public static final String MSG_TAG = "TETHER -> NativeTask";
+	
+	static {
         try {
-            Log.i("JNI", "Trying to load libNativeTask.so");
-            //System.loadLibrary("NativeTask");
+            Log.i(MSG_TAG, "Trying to load libNativeTask.so");
             System.load("/data/data/android.tether.usb/library/libNativeTask.so");
         }
         catch (UnsatisfiedLinkError ule) {
-            Log.e("JNI", "WARNING: Could not load libNativeTask.so");
+            Log.e(MSG_TAG, "Could not load libNativeTask.so");
         }
     }
     public static native String getProp(String name);
     public static native int runCommand(String command);
-    
 }
+
